5b000e0fb4552bbadb5843fcd114287761fc4423
kowalsska <kowalsska2@gmail.com>
2021-02-24 15:14:22 -1000

    Added query param to traverse the directory

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # exercise_app
 
-To run the app:
+To run the app run this in the root directory of the project:
 `bash start.sh`
-
 And follow the prompts
+
+
+# Instructions
+
+Use query param `q` to traverse the root directory. For example:
+`http://0.0.0.0:8000/?q=Documents/house/cat.txt`
+
+
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-from typing import Optional
-
-from os import path, listdir, walk
-from os.path import isfile, join, exists
-
-from fastapi import FastAPI
+import json
 import uvicorn
+
+from os import path, listdir, walk, stat
+from os.path import isfile, join, exists, splitext, isdir
+
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -12,33 +14,91 @@ HOME_DIR = ""
 FILES_ROOT = "/usr/files"
 
 
-def set_home_dir(dir):
+def set_home_dir(dir:str):
     global HOME_DIR
     HOME_DIR = FILES_ROOT
     if dir:
         HOME_DIR = "{}/{}".format(HOME_DIR, dir)
     print("HOME_DIR is: {}".format(HOME_DIR))
 
+
 @app.get("/")
-def listfiles():
-    print("HOME_DIR is: {}".format(HOME_DIR))
-    return get_files(HOME_DIR)
+def listfiles(q:str = Query(None)):
+    path = HOME_DIR
+    if q:
+        path = join(HOME_DIR, q)
+    files = get_details(path)
+    return JSONResponse(content=files)
 
 
-@app.get("/{subdir}")
-def listfiles(subdir):
-    path = "{}/{}".format(HOME_DIR, subdir)
-    return get_files(path)
-
-
-def get_files(path):
+def get_details(path):
     print("List files at: {}".format(path))
-    if exists(path):
-        return listdir(path)
+    
+    filename, file_extension = splitext(path)
+    if file_extension and file_extension != ".txt":
+        raise HTTPException(status_code=404, detail="Can only read .txt files")
+    
+    if not exists(path):
+        raise HTTPException(
+            status_code=404, detail="The path `{}` does not exist.".format(path)
+        )
+
+    if isdir(path):
+        return get_dir(path)
+    else:
+        return get_file(path)
 
 
+def get_dir(path):
+    items = listdir(path)
+    items_list = []
+    for item in items:
+        item_path = join(path, item)
+        item_stat = stat(item_path)
 
-if __name__ == '__main__':
+        owner, size, permissions = None, None, None
+        if item_stat:
+            owner, size, permissions = (
+                item_stat.st_uid,
+                item_stat.st_size,
+                item_stat.st_mode,
+            )
+
+        items_list.append({
+                "name": item, 
+                "owner": owner, 
+                "size": size, 
+                "permissions": permissions
+            }
+        )
+
+    return jsonable_encoder(items_list)
+
+
+def get_file(path):
+    filename, file_extension = splitext(path)
+
+    with open(path) as f:
+        content = f.read()
+
+    item_stat = stat(path)
+    owner, size, permissions = (
+        item_stat.st_uid,
+        item_stat.st_size,
+        item_stat.st_mode,
+    )
+    return jsonable_encoder(
+        {
+            "name": filename,
+            "owner": owner,
+            "size": size,
+            "permissions": permissions,
+            "content": content,
+        }
+    )
+
+
+if __name__ == "__main__":
     dir = input("Welcome. Please specify a root directory:")
     print("OK! " + dir)
     set_home_dir(dir)
